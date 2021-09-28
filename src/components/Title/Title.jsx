163f59f732be29c5df466a1d71be2db6d3fdeb3c
Adam <sanktimarus@gmail.com>
2021-09-28 12:07:53 +0200

    Attempting the Title component's creation

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+
+export function Title() {
+    return (
+        <>
+            <h1>
+                Wumsz TV
+            </h1>
+            <p>
+                Dom Kumpli z Kanapy na Plaży
+            </p>
+        </>
+    );
+}
